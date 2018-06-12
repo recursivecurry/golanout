402a6c3a0306d75f199276f1dfb5a8457a9b1a1e
Jongsoo Lee <leejongsoo@gmail.com>
2018-06-13 02:53:03 +0900

    add max/min

--- experiment/pure/min.go
+++ experiment/pure/min.go
@@ -1,1 +1,42 @@
 package pure
+
+import (
+	"github.com/recursivecurry/golanout/experiment/base"
+	"errors"
+)
+
+type Min struct {
+
+	values []Interface
+}
+
+func (m Min) Run(inputs base.Inputs, params base.Params, salt base.Salt) (base.Value, error) {
+
+	if len(m.values) == 0 {
+		return nil, errors.New("no values")
+	}
+	var min float64
+	value, err := m.values[0].Value(inputs, params, salt)
+	if err != nil {
+		return nil, err
+	}
+	if fv, ok := value.(float64); ok {
+		min = fv
+	}
+	for _, v := range m.values[1:] {
+		value, err := v.Value(inputs, params, salt)
+		if err != nil {
+			return nil, err
+		}
+		if fv, ok := value.(float64); ok {
+			if fv < min {
+				min = fv
+			}
+		}
+	}
+	return min, nil
+}
+
+func (Min) Name() base.Operator {
+	return "min"
+}
